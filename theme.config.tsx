5eadf6103e55f4f7d362458feda5d71b55f40290
Ronier Lima <ronier.lim@gmail.com>
2023-04-21 13:48:23 -0300

    aula de react

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,14 +2,14 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>FS11</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/ronierlima/digitalcollege-docs',
   },
   chat: {
     link: 'https://discord.com',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/ronierlima/digitalcollege-docs',
   footer: {
     text: 'Nextra Docs Template',
   },
