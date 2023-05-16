e4667c707c00e45b0387ee76a369de9cf9c4368c
Ronier Lima <ronier.lim@gmail.com>
2023-05-16 14:41:44 -0300

    fechando estrutura

--- theme.config.tsx
+++ theme.config.tsx
@@ -11,7 +11,7 @@ const config: DocsThemeConfig = {
   },
   docsRepositoryBase: 'https://github.com/ronierlima/digitalcollege-docs',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'By Ronier Lima',
   },
 }
 
