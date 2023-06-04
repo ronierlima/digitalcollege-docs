b9e4419a758d200ce145c3629f7ce3f295655e80
Ronier Lima <ronier.lim@gmail.com>
2023-06-03 22:11:27 -0300

    ajusta head

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import { useRouter } from 'next/router'
+import { DocsThemeConfig, useConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
   logo: <span>FS11</span>,
@@ -13,6 +14,24 @@ const config: DocsThemeConfig = {
   footer: {
     text: 'By Ronier Lima',
   },
+  head: () => {
+    const { asPath, defaultLocale, locale } = useRouter()
+    const { frontMatter } = useConfig()
+    const url =
+      'https://fs11.ronierlima.dev' +
+      (defaultLocale === locale ? asPath : `/${locale}${asPath}`)
+ 
+    return (
+      <>
+        <meta property="og:url" content={url} />
+        <meta property="og:title" content={frontMatter.title || 'FS11'} />
+        <meta
+          property="og:description"
+          content={frontMatter.description || 'Página de documentação para turma fs11'}
+        />
+      </>
+    )
+  }
 }
 
 export default config
